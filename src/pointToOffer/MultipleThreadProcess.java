98e065a9ce5bd7711bc2585413389b2e121b2ac6
chengnan <chengnan@staff.weibo.com>
2021-03-10 16:31:34 +0800

    many

--- src/pointToOffer/MultipleThreadProcess.java
+++ src/pointToOffer/MultipleThreadProcess.java
@@ -1,2 +1,47 @@
-package pointToOffer;public class MultipleThreadProcess {
+package pointToOffer;
+
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+import java.util.Arrays;
+
+public class MultipleThreadProcess {
+    private static final int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    private static final int threadCount = 3;
+
+    public static int fun(){
+        return Integer.valueOf("1")- Integer.valueOf("12");
+    }
+    public static void main(String[] args) {
+        System.out.println(1<<1);
+    }
+
+    public static void main1(String[] args) {
+        int length = array.length;
+        int start = 0;
+        int divide = length / threadCount;
+        for (int i = 0; i < threadCount; i++) {
+            int[] subArray;
+            if (i < threadCount - 1) {
+                subArray = Arrays.copyOfRange(array, start + i * divide, start + (i + 1) * divide);
+                System.out.println(i + ":" + Arrays.stream(subArray).toArray().toString());
+            } else {
+                subArray = Arrays.copyOfRange(array, start + i * divide, length - 1);
+                System.out.println(i + ":" + Arrays.stream(subArray).toArray().toString());
+            }
+
+            new Thread(() -> {
+                for (int j = 0; j < subArray.length; j++) {
+                    if (subArray[j] % 15 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "nums:" + subArray[j] + " A ");
+                    } else if (subArray[j] % 5 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "nums:" + subArray[j] + " B ");
+                    } else if (subArray[j] % 3 == 0) {
+                        System.out.println(Thread.currentThread().getName() + "nums:" + subArray[j] + " C ");
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + "nums:" + subArray[j] + " D ");
+                    }
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
 }
--- src/pointToOffer/QuickSortFIndKmax.java
+++ src/pointToOffer/QuickSortFIndKmax.java
@@ -1,2 +1,50 @@
-package pointToOffer;public class QuickSortFIndKmax {
+package pointToOffer;
+
+import java.util.Arrays;
+
+public class QuickSortFIndKmax {
+
+    //通过快速排序的枢值 如果枢值== arr.length-i 则返回
+    //小于则往右边走 同时arr=右边的数组，num不变
+    //大于则往左边走 同时arr=左边的数组，num=arr.length-num
+    public static int quickSort(int arr[], int num) {
+        int target = arr[0];
+        int i = 1, j = arr.length - 1, tmp;
+        while (i < j) {
+            while (i < j) {
+                if (arr[i] > target)
+                    break;
+                i++;
+            }
+            while (i < j) {
+                if (arr[j] < target)
+                    break;
+                j--;
+            }
+            if (i < j) {
+                tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+        if (num == arr.length - i + 1)
+            return target;
+        if (num > arr.length - i + 1) {
+            int[] sub = new int[i - 1];
+            System.arraycopy(arr, 1, sub, 0, i - 1);
+            return quickSort(sub, num - (arr.length - i) - 1);
+        }
+        if (num < arr.length - i + 1) {
+            int[] sub = new int[arr.length - i];
+            System.arraycopy(arr, i, sub, 0, arr.length - i);
+            return quickSort(sub, num);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //int[] array={5,3,4,2,1,23,12,11,6};//第5大的数是5
+        int[] array = {5, 1, 2, 3, 0, -1, 7, 6};//第5大的数是5
+        System.out.println(quickSort(array, 4));
+    }
 }
